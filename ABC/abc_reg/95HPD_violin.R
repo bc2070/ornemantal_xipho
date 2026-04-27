57f1088e5c9e3b9f9b86414b69e03d41022af0e8
bc2070 <119569139+bc2070@users.noreply.github.com>
2026-04-27 09:25:07 +0800

    Update 95HPD_violin.R

--- ABC/abc_reg/95HPD_violin.R
+++ ABC/abc_reg/95HPD_violin.R
@@ -25,30 +25,22 @@ dat1$lambda <- dat1$popsize2 / dat1$l
 dat1_new <- dat1[,c("gen","lambda","Ne","s","pop")]
 
 library(ggplot2)
-# 直方图
 ggplot(dat1_new, aes(x = s)) +
   geom_histogram(binwidth = 0.1, fill = "skyblue", color = "black") +
   labs(title = "Distribution of s", x = "s", y = "Frequency") +
   theme_minimal() +
   xlim(-1, 1)  
 
-# 密度图
 ggplot(dat1_new, aes(x = s)) +
   geom_density(fill = "skyblue", alpha = 0.5) +
   labs(title = "Density Plot of s", x = "s", y = "Density") +
   theme_minimal() + 
   xlim(-1, 1)
 
-###扫描峰值
-
-
-# 计算密度
 dens <- density(dat1_new$s)
 
-# 找到峰值
 peak <- dens$x[which.max(dens$y)]
 
-# 绘制图形并标注峰值
 ggplot(dat1_new, aes(x = s)) +
   geom_density(fill = "skyblue", alpha = 0.5) +
   geom_vline(xintercept = peak, color = "red", linetype = "dashed") +
@@ -63,25 +55,19 @@ ggplot(dat1_new, aes(x = s)) +
 ###
 #lambda
 ###
-# 直方图
 ggplot(dat1_new, aes(x = lambda)) +
   geom_histogram(binwidth = 0.1, fill = "skyblue", color = "black") +
   labs(title = "Distribution of lambda", x = "lambda", y = "Frequency") +
   theme_minimal()
 
-# 密度图
 ggplot(dat1_new, aes(x = lambda)) +
   geom_density(fill = "skyblue", alpha = 0.5) +
   labs(title = "Density Plot of lambda", x = "lambda", y = "Density") +
   theme_minimal()
-
-# 计算密度
 dens <- density(dat1_new$lambda)
 
-# 找到峰值
 peak <- dens$x[which.max(dens$y)]
 
-# 绘制图形并标注峰值
 ggplot(dat1_new, aes(x = lambda)) +
   geom_density(fill = "skyblue", alpha = 0.5) +
   geom_vline(xintercept = peak, color = "red", linetype = "dashed") +
@@ -94,13 +80,12 @@ ggplot(dat1_new, aes(x = lambda)) +
 ####
 #gen
 ####
-# 直方图
+
 ggplot(dat1_new, aes(x = gen)) +
   geom_histogram(binwidth = 0.1, fill = "skyblue", color = "black") +
   labs(title = "Distribution of T", x = "T", y = "Frequency") +
   theme_minimal()
 
-# 密度图
 ggplot(dat1_new, aes(x = gen)) +
   geom_density(fill = "skyblue", alpha = 0.5) +
   labs(title = "Density Plot of T", x = "T", y = "Density") +
@@ -109,13 +94,11 @@ ggplot(dat1_new, aes(x = gen)) +
 ####
 #Ne
 ####
-# 直方图
 ggplot(dat1_new, aes(x = Ne)) +
   geom_histogram(binwidth = 0.1, fill = "skyblue", color = "black") +
   labs(title = "Distribution of Ne", x = "Ne", y = "Frequency") +
   theme_minimal()
 
-# 密度图
 ggplot(dat1_new, aes(x = Ne)) +
   geom_density(fill = "skyblue", alpha = 0.5) +
   labs(title = "Density Plot of Ne", x = "Ne", y = "Density") +
